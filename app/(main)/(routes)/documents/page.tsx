672c00c38a5ae52aa8285373d83d918375b8391e
duy1111 <maixuanduy0605@gmail.com>
2023-11-11 22:22:49 +0700

    setup base note

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return ( 
+        <div>
+            This it protected Documents Page
+        </div>    
+    );
+}
+ 
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+    providers: [
+      {
+        domain: "https://gentle-buzzard-6.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
