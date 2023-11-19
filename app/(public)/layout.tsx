3a817b41a0d9d51d7fa5f255dcf6a00ab5a858a9
duy1111 <maixuanduy0605@gmail.com>
2023-11-19 15:27:55 +0700

    publish functionality #2

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,13 @@
+const PublishLayout = ({
+    children
+}:{
+    children:React.ReactNode
+}) => {
+    return ( 
+        <div className="h-full dark:bg-[#1F1F1F]" >
+            {children}
+        </div>
+     );
+}
+ 
+export default PublishLayout;
